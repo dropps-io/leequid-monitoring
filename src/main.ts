7b65bb2aad92e9b2c39999b2a31263b3a6c823a7
Samuel VIDEAU <70749906+samuel-videau@users.noreply.github.com>
2024-01-26 16:04:40 +1100

    release (#4)
    * chore: cors enabled for non prod env
    
    * chore: cors enabled for non prod env

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {seedMonitoring} from "./db-client/seed";
-import {PORT} from "./globals";
+import {DEPLOY_ENV, PORT} from "./globals";
 
 async function bootstrap() {
   await seedMonitoring();
   const app = await NestFactory.create(AppModule);
+
+  if (DEPLOY_ENV !== 'prod') {
+    app.enableCors({
+      origin: 'http://localhost:3000',
+      methods: 'GET',
+    });
+  }
+
   await app.listen(PORT);
 }
 bootstrap();

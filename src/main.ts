092d450a299aef3cc155cceb568ef3077094329e
samuel-videau <samuel.videau@efrei.net>
2024-01-27 23:28:50 +1100

    chore: cors config

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,17 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { seedMonitoring } from './db-client/seed';
-import { DEPLOY_ENV, PORT } from './globals';
+import { PORT } from './globals';
 
 async function bootstrap(): Promise<void> {
   await seedMonitoring();
 
   const app = await NestFactory.create(AppModule);
 
-  if (DEPLOY_ENV !== 'prod') {
-    app.enableCors({
-      origin: ['http://localhost:3000', 'http://localhost:3001', 'https://staging.app.leequid.io'],
-      methods: 'GET',
-    });
-  } else {
-    app.enableCors({
-      origin: ['https://app.leequid.io'],
-      methods: 'GET',
-    });
-  }
+  app.enableCors({
+    origin: ['https://staging.app.leequid.io', 'https://app.leequid.io'],
+    methods: 'GET',
+  });
 
   await app.listen(PORT);
 }
